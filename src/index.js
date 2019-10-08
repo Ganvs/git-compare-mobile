573c99d2ba89d4951315c6a8597d9f95048917b8
Ganvs <gustavo.anverze@gmail.com>
2019-10-08 19:27:07 -0300

    navigation added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello World</Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-console.tron.log('teste tron');
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
